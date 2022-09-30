186d0fa61928899d8dc95b61bc89b15964055b28
Luc van Bree <luc-van-bree@hotmail.com>
2022-09-30 16:55:53 +0200

    changed the filepaths

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,18 +1,18 @@
 library(tidyverse)
 
-n_max = Inf 
+n_max = Inf
 
 all_listings_csv_links = c('http://data.insideairbnb.com/australia/vic/melbourne/2022-03-09/data/listings.csv.gz',  
-                  'http://data.insideairbnb.com/australia/nsw/sydney/2022-03-09/data/listings.csv.gz',
-                  'http://data.insideairbnb.com/spain/catalonia/barcelona/2022-03-10/data/listings.csv.gz',
-                  'http://data.insideairbnb.com/spain/comunidad-de-madrid/madrid/2022-03-10/data/listings.csv.gz')
+                           'http://data.insideairbnb.com/australia/nsw/sydney/2022-03-09/data/listings.csv.gz',
+                           'http://data.insideairbnb.com/spain/catalonia/barcelona/2022-03-10/data/listings.csv.gz',
+                           'http://data.insideairbnb.com/spain/comunidad-de-madrid/madrid/2022-03-10/data/listings.csv.gz')
 
 
 all_calendar_csv_links = c('http://data.insideairbnb.com/australia/vic/melbourne/2022-03-09/data/calendar.csv.gz',
-                 'http://data.insideairbnb.com/australia/nsw/sydney/2022-03-09/data/calendar.csv.gz',
-                 'http://data.insideairbnb.com/spain/catalonia/barcelona/2022-03-10/data/calendar.csv.gz',
-                 'http://data.insideairbnb.com/spain/comunidad-de-madrid/madrid/2022-03-10/data/calendar.csv.gz')
-                 
+                           'http://data.insideairbnb.com/australia/nsw/sydney/2022-03-09/data/calendar.csv.gz',
+                           'http://data.insideairbnb.com/spain/catalonia/barcelona/2022-03-10/data/calendar.csv.gz',
+                           'http://data.insideairbnb.com/spain/comunidad-de-madrid/madrid/2022-03-10/data/calendar.csv.gz')
+
 # make list of the dataframes
 all_calendar_list <- lapply(all_calendar_csv_links, read_csv, n_max = n_max)
 all_listings_list <- lapply(all_listings_csv_links, read_csv, n_max = n_max)
@@ -33,8 +33,8 @@ all_listings <-  bind_rows(cbind(city='madrid', all_listings_list[[4]]),
 
 
 # write as csv
-write.csv(all_calendar,"all_calendar.csv")
-write.csv(all_listings,"all_listings.csv")
+write.csv(all_calendar,"../../data/all_calendar.csv")
+write.csv(all_listings,"../../data/all_listings.csv")
 
 
 
