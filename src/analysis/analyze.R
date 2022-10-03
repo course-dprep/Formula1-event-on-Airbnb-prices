5cb8b23a4a71cc9c51c39001cc797b3765d4b043
sjoerdbijlsb <s.bijl_1@tilburguniversity.edu>
2022-10-03 15:35:11 +0200

    added models but need to add covoriates still

--- src/analysis/analyze.R
+++ src/analysis/analyze.R
@@ -1,11 +1,55 @@
+library(fixest)
+
 # load 
-load("./gen/analysis/input/data_cleaned.RData")
+final <-  read.csv("../data-preparation/final_airbnb_dataset.csv")
 
-# Estimate model 1 
-m1 <- lm(V1 ~ V3 + V4,df_cleaned)
+# filter on melbourne/sydney
+australia <- c("melbourne", "sydney")
+spain <- c("madrid", "barcelona")
 
-# Estimate model 2 
-m2 <- lm(V1 ~ V3 + V4 + V5 , df_cleaned)
+filter_australia <- filter(final, city == australia) %>% 
+filter(date > "2022-03-25") %>% 
+filter(date < "2022-04-11")
 
-# Save results
-save(m1,m2,file="./gen/analysis/output/model_results.RData")
+filter_spain <-  filter(final, city == spain)
+filter(date > "2022-05-05") %>% 
+filter(date < "2022-05-23")
+
+# make new column for dummy variable
+filter_australia$dummy <- ifelse(filter_australia$city == "melbourne", 1, 0)
+# make new column for dummy variable formula 1 period
+filter_australia$formula1 <- ifelse(filter_australia$date > "2022-04-07", 1, 0)
+
+# make new column for dummy variable
+filter_spain$dummy <- ifelse(filter_spain$city == "barcelona", 1, 0)
+# make new column for dummy variable formula 1 period
+filter_spain$formula1 <- ifelse(filter_spain$date > "2022-05-19", 1, 0)
+
+
+# Estimate model 1 (Australia)
+model_1 <- feols(price ~ formula1 + dummy + 
+                formula1:dummy,
+                 data = filter_australia,
+                 cluster = ~ id)
+
+# Estimate model 1 (Spain)
+model_2 <- feols(price ~ formula1 + dummy + 
+                   formula1:dummy,
+                 data = filter_spain,
+                 cluster = ~ id)
+
+
+
+# make output table in modelsummary 
+
+models  <- list('Australia' = model_1, 
+                 'Spain' =  model_2)
+          
+
+modelsummary(models)
+
+
+
+
+
+
