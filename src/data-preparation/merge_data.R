ca9aca760cdb375d5214363d5ab5d09375574bce
Luc van Bree <luc-van-bree@hotmail.com>
2022-10-04 10:39:29 +0200

    create one csv of the all_listings and all_calendar

--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -1,11 +1,11 @@
 library(dplyr)
 # Load datasets into R 
-all_listings <- read.csv("../../data/all_listings.csv")
-all_calendar <- read.csv("../../data/all_calendar.csv")
+all_listings <- read.csv("../../gen/data-preparation/temp/all_listings.csv")
+all_calendar <- read.csv("../../gen/data-preparation/temp/all_calendar.csv")
 
 # Delete automatic generated column "X"
-all_listings <- subset(all_listings, select = -c(X))
-all_calendar <- subset(all_calendar, select = -c(X))
+all_listings <- subset(all_listings, select = -c(X, X.1))
+all_calendar <- subset(all_calendar, select = -c(X, X.1))
 
 # Change column name of "listing_id" to "id"
 colnames(all_calendar)[2] <- "id"
