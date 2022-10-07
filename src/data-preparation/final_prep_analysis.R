2d8bba79622d30a0138abfefbaeb51cb96232133
sjoerdbijlsb <s.bijl_1@tilburguniversity.edu>
2022-10-07 20:17:53 +0200

    had to add a few more lines that were initially in the analysis folder

--- src/data-preparation/final_prep_analysis.R
+++ src/data-preparation/final_prep_analysis.R
@@ -11,9 +11,23 @@ filtered_australia <- filter(final_airbnb_dataset, city == australia) %>%
   filter(date > "2022-03-25") %>% 
   filter(date < "2022-04-11")
 
+# make new column for event_city variable 
+filtered_australia$event_city <- ifelse(filtered_australia$city == "melbourne", 1, 0)
+# make new column for event_city variable formula 1 period
+filtered_australia$formula1 <- ifelse(filtered_australia$date > "2022-04-07", 1, 0)
+
 filtered_spain <- filter(final_airbnb_dataset, city == spain) %>% 
   filter(date > "2022-05-05") %>% 
   filter(date < "2022-05-23")
+
+
+# make new column for event_city variable
+filtered_spain$event_city <- ifelse(filtered_spain$city == "barcelona", 1, 0)
+
+# make new column for event_city variable formula 1 period
+filtered_spain$formula1 <- ifelse(filtered_spain$date > "2022-05-19", 1, 0)
+
+
 
 write.csv(filtered_spain, "../../gen/data-preparation/output/filtered_spain.csv")
 write.csv(filtered_australia, "../../gen/data-preparation/output/filtered_australia.csv")
