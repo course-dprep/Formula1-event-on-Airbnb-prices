8f06326ce6d990136dda92be54ef4ba5b69ed7fe
SamVillevoye <s.a.l.p.villevoije@tilburguniversity.edu>
2022-10-10 23:48:56 +0200

    New way to change colnames

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -7,10 +7,10 @@ merged_airbnb_data <- read_csv("../../gen/data-preparation/temp/merged_airbnb_da
 merged_airbnb_data <- subset(merged_airbnb_data, select = -c(...1, city.y, price.y, minimum_nights.y, maximum_nights.y))
 
 # Adjusting column names 
-colnames(merged_airbnb_data)[1] <- "city"
-colnames(merged_airbnb_data)[4] <- "price"
-colnames(merged_airbnb_data)[5] <- "minimum_nights"
-colnames(merged_airbnb_data)[6] <- "maximum_nights"
+colnames(all_calendar)[which(names(all_calendar) == "city.x")] <- "city"
+colnames(all_calendar)[which(names(all_calendar) == "price.x")] <- "price"
+colnames(all_calendar)[which(names(all_calendar) == "minimum_nights.x")] <- "minimum_nights"
+colnames(all_calendar)[which(names(all_calendar) == "maximum_nights.x")] <- "maximum_nights"
 
 # Save cleaned data
 write.csv(merged_airbnb_data, "../../gen/data-preparation/temp/cleaned_airbnb_data.csv")
