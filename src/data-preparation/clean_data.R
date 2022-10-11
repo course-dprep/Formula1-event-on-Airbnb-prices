f4a614ef92c7426201f10f59a2042d2b25e1c2ee
Bjorn <109277196+BjornLauwers@users.noreply.github.com>
2022-10-11 11:32:46 +0200

    Update clean_data.R

--- src/data-preparation/clean_data.R
+++ src/data-preparation/clean_data.R
@@ -7,10 +7,10 @@ merged_airbnb_data <- read_csv("../../gen/data-preparation/temp/merged_airbnb_da
 merged_airbnb_data <- subset(merged_airbnb_data, select = -c(...1, city.y, price.y, minimum_nights.y, maximum_nights.y))
 
 # Adjusting column names 
-colnames(all_calendar)[which(names(all_calendar) == "city.x")] <- "city"
-colnames(all_calendar)[which(names(all_calendar) == "price.x")] <- "price"
-colnames(all_calendar)[which(names(all_calendar) == "minimum_nights.x")] <- "minimum_nights"
-colnames(all_calendar)[which(names(all_calendar) == "maximum_nights.x")] <- "maximum_nights"
+colnames(merged_airbnb_data)[which(names(merged_airbnb_data) == "city.x")] <- "city"
+colnames(merged_airbnb_data)[which(names(merged_airbnb_data) == "price.x")] <- "price"
+colnames(merged_airbnb_data)[which(names(merged_airbnb_data) == "minimum_nights.x")] <- "minimum_nights"
+colnames(merged_airbnb_data)[which(names(merged_airbnb_data) == "maximum_nights.x")] <- "maximum_nights"
 
 # Save cleaned data
 write.csv(merged_airbnb_data, "../../gen/data-preparation/temp/cleaned_airbnb_data.csv")
