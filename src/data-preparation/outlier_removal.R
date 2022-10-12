9e10da7ac1e8b4a6ec39c09a3077993e12cc712d
Luc van Bree <luc-van-bree@hotmail.com>
2022-10-12 11:14:35 +0200

    changed the file destination and filename that is written in this script

--- src/data-preparation/outlier_removal.R
+++ src/data-preparation/outlier_removal.R
@@ -5,11 +5,6 @@ cleaned_airbnb_data <- read.csv("../../gen/data-preparation/temp/cleaned_airbnb_
 # Delete automatic generated column "X"
 cleaned_airbnb_data <- subset(cleaned_airbnb_data, select = -c(X))
 
-# deleting all prices = 0
-
-# cleaned_airbnb_data <- cleaned_airbnb_data %>% filter(price < ???)
-
-
 # remove the outliers
 quartiles <- quantile(cleaned_airbnb_data$price, probs=c(.25, .75), na.rm = TRUE)
 IQR <- IQR(cleaned_airbnb_data$price, na.rm = TRUE)
@@ -19,10 +14,5 @@ Upper <- quartiles[2] + 1.5*IQR
 
 cleaned_airbnb_data_without_outliers <- cleaned_airbnb_data %>% filter(price > Lower & price < Upper)
 
-
-# NA's all columns
-#cleaned_airbnb_data_without_outliers %>% 
-  #summarise_all(~sum(is.na(.)))
-
-write.csv(cleaned_airbnb_data_without_outliers, "../../gen/data-preparation/output/final_airbnb_dataset.csv")
+write.csv(cleaned_airbnb_data_without_outliers, "../../gen/data-preparation/temp/airbnb_dataset_without_outliers.csv")
 
