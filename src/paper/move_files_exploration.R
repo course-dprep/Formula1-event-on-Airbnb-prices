8297a54fa73cf77ade075ca77baf30565abded9e
sjoerdbijlsb <s.bijl_1@tilburguniversity.edu>
2022-10-13 23:17:38 +0200

    added the files to fully execute the markdown files. Updated the make file to let the output be the gen directory.

---
+++ src/paper/move_files_exploration.R
@@ -0,0 +1,4 @@
+file.copy(from = paste0("exploration.pdf"),
+          to = paste0("../../gen/paper/output/exploration.pdf"))
+
+file.remove(from = paste0("exploration.pdf"))
---
+++ src/paper/move_files_manuscript_analysis.R
@@ -0,0 +1,4 @@
+file.copy(from = paste0("manuscript_analysis.pdf"),
+          to = paste0("../../gen/paper/output/manuscript_analysis.pdf"))
+
+file.remove(from = paste0("manuscript_analysis.pdf"))
