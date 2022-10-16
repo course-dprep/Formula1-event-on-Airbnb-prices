ea803ef0ed3e053b6b56067d325c03d100a8cc7d
sjoerdbijlsb <s.bijl_1@tilburguniversity.edu>
2022-10-16 08:47:53 +0200

    changed plot titles through column and row names in this plot only

--- src/analysis/plot_australia.R
+++ src/analysis/plot_australia.R
@@ -10,13 +10,19 @@ grp_avg_australia <-
   group_by(event_city, date) %>%
   summarise(price = mean(price)) %>%
   arrange(date, event_city)
+  
+#rename column and row names
+colnames(grp_avg_australia) <-  c("city", "date", "price")
+grp_avg_australia$city[grp_avg_australia$city == '0'] <- 'Sydney'
+grp_avg_australia$city[grp_avg_australia$city == '1'] <- 'Melbourne'
+
 
 
 # check assumptions by plotting price over time
 #plot australia
 #pdf("plot_australia.pdf")
-ggplot(grp_avg_australia, aes(x = date, y = price, color = factor(event_city), group = event_city )) + 
-  geom_line() 
+ggplot(grp_avg_australia, aes(x = date, y = price, color = factor(city), group = city )) + 
+  geom_line() + scale_colour_discrete("Continents")
 
 ggsave(filename = "../../gen/analysis/output/plot_australia.png", width = 15, height = 6, dpi = 100, units = "cm") 
 
--- src/analysis/plot_spain.R
+++ src/analysis/plot_spain.R
@@ -11,11 +11,15 @@ grp_avg_spain <-
   arrange(date, event_city)
 
 
+#rename column and row names
+colnames(grp_avg_spain) <-  c("city", "date", "price")
+grp_avg_spain$city[grp_avg_spain$city == '0'] <- 'Madrid'
+grp_avg_spain$city[grp_avg_spain$city == '1'] <- 'Barcelona'
 
 # check assumptions by plotting price over time
 #plot spain 
 
-ggplot(grp_avg_spain, aes(x = date, y = price, color = factor(event_city), group = event_city )) + 
-  geom_line() 
+ggplot(grp_avg_spain, aes(x = date, y = price, color = factor(city), group = city )) + 
+  geom_line() + scale_colour_discrete("City")
 
 ggsave(filename = "../../gen/analysis/output/plot_spain.png",  width = 15, height = 6, dpi = 100, units = "cm")
